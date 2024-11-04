57ee2e7a3afd25ee13e28b22cc7f55fef93a6492
nychoi2 <git@stackblitz.com>
2024-11-03 23:53:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-easygame
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nychoi2/stackblitz-easygame)
---
+++ script.js
@@ -0,0 +1,28 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    character.style.top = '120px'
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+}
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'Space') {
+      startJump();
+  }
+});
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+           block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);  
